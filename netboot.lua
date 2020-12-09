39d246a40ebca51a3740be8c6936f1b5255af852
Chris <chrissx@chrissx.de>
2020-12-09 19:39:23 +0100

    error handling for netboot

--- netboot.lua
+++ netboot.lua
@@ -3,6 +3,12 @@
 local SERVER = "https://chrissxyt.github.io/cpm/"
 local SUPPORT = "Twitter @chrissxMedia"
 
+if not http then
+        print("The http package isn't enabled in your ComputerCraft-config.")
+        print("Please enable it, restart Minecraft and rerun the cpm installer.")
+        return
+end
+
 local res = http.get(SERVER.."cpm.lua")
 if not res then
         print("Cannot reach the server. ("..SERVER..")")
@@ -11,6 +17,7 @@ if not res then
 end
 local cpm = res.readAll()
 res.close()
+
 local f = fs.open("cpm", "w")
 f.write(cpm)
 f.close()
