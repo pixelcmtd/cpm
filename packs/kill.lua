57de16cc5e332138202e1c230a0fde4aac084752
chrissx <chrissx@chrissx.de>
2021-03-26 15:32:15 +0100

    added a program i wrote for a thing

---
+++ packs/kill.lua
@@ -0,0 +1,11 @@
+while true do
+        turtle.attack()
+        turtle.drop()
+        turtle.select(2)
+        turtle.drop()
+        turtle.select(3)
+        turtle.drop()
+        turtle.select(4)
+        turtle.drop()
+        turtle.select(1)
+end
