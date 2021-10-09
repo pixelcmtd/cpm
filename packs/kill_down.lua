7e12a154e82d5e5b5fc5f1da5a451cfb7e8242c3
chris <chrissx@chrissx.de>
2021-10-09 16:26:41 +0200

    removed dropping from kill_down
    it just makes more sense this way

--- packs/kill_down.lua
+++ packs/kill_down.lua
@@ -1,11 +1,3 @@
 while true do
         turtle.attackDown()
-        turtle.dropDown()
-        turtle.select(2)
-        turtle.dropDown()
-        turtle.select(3)
-        turtle.dropDown()
-        turtle.select(4)
-        turtle.dropDown()
-        turtle.select(1)
 end
